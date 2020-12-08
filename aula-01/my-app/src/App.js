55e6c8744f8c1e329f6d2c80d39be7dc15cece50
JOGP (Jorge Gustavo Brant Cangussu e Pinho) <JOGP@novonordisk.com>
2020-12-08 16:49:37 -0300

    stateFull

--- aula-01/my-app/src/App.js
+++ aula-01/my-app/src/App.js
@@ -4,11 +4,39 @@ import './App.css';
 import { render } from 'react-dom';
 
 class App extends React.Component {
+  constructor () {
+    super();
+
+    this.state = {
+      data: 
+      [
+        {
+          "id": 1,
+          "name": "Foo",
+          "age": "20"
+        },
+        {
+          "id": 2,
+          "name": "Bar",
+          "age": "30"
+        },
+        {
+          "id": 3,
+          "name": "Baz",
+          "age": "40"
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div>
         <Header />
-        <Content />
+        <table>
+          <tbody>
+            { this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
+          </tbody>
+        </table>
       </div>
     );
   }
@@ -31,6 +59,18 @@ class Content extends React.Component {
         <h2>Content</h2>
         <p>The content text!!!</p>
       </div>
+    );
+  }
+}
+
+class TableRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{ this.props.data.id }</td>
+        <td>{ this.props.data.name }</td>
+        <td>{ this.props.data.age }</td>
+      </tr>
     );
   }
 }
