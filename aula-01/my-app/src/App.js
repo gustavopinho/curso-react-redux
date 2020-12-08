70412f4c8040389b912b0145c3fe412d635219eb
JOGP (Jorge Gustavo Brant Cangussu e Pinho) <JOGP@novonordisk.com>
2020-12-08 17:11:41 -0300

    props

--- aula-01/my-app/src/App.js
+++ aula-01/my-app/src/App.js
@@ -4,73 +4,12 @@ import './App.css';
 import { render } from 'react-dom';
 
 class App extends React.Component {
-  constructor () {
-    super();
-
-    this.state = {
-      data: 
-      [
-        {
-          "id": 1,
-          "name": "Foo",
-          "age": "20"
-        },
-        {
-          "id": 2,
-          "name": "Bar",
-          "age": "30"
-        },
-        {
-          "id": 3,
-          "name": "Baz",
-          "age": "40"
-        }
-      ]
-    };
-  }
-  render() {
+    render() {
     return (
       <div>
-        <Header />
-        <table>
-          <tbody>
-            { this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
-          </tbody>
-        </table>
+        <h1>{this.props.headerProp}</h1>
+        <h1>{this.props.contentProp}</h1>
       </div>
-    );
-  }
-}
-
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Header</h1>
-      </div>
-    );
-  }
-}
-
-class Content extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Content</h2>
-        <p>The content text!!!</p>
-      </div>
-    );
-  }
-}
-
-class TableRow extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td>{ this.props.data.id }</td>
-        <td>{ this.props.data.name }</td>
-        <td>{ this.props.data.age }</td>
-      </tr>
     );
   }
 }
--- aula-01/my-app/src/index.js
+++ aula-01/my-app/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App headerProp="Header from props..." contentProp="Content from props..." />
   </React.StrictMode>,
   document.getElementById('root')
 );
