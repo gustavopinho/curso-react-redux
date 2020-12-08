a0777f79f7bebfccbb328b07971af1208fec47c8
JOGP (Jorge Gustavo Brant Cangussu e Pinho) <JOGP@novonordisk.com>
2020-12-08 15:47:41 -0300

    Props

--- aula-01/my-app/src/index.js
+++ aula-01/my-app/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App subject="Teste"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
