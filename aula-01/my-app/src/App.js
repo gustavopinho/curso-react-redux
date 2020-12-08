9cdce187247ba604b63b90b66c69798e7c7b439b
JOGP (Jorge Gustavo Brant Cangussu e Pinho) <JOGP@novonordisk.com>
2020-12-08 17:32:40 -0300

    Estado (state) e props

--- aula-01/my-app/src/App.js
+++ aula-01/my-app/src/App.js
@@ -4,19 +4,42 @@ import './App.css';
 import { render } from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        header: "Header from props...",
+        content: "Content from props..."
+      };
+    }
     render() {
     return (
       <div>
-        <h1>{this.props.headerProp}</h1>
-        <h1>{this.props.contentProp}</h1>
+        <Header headerProp={this.state.header} />
+        <Content contentProp={this.state.content} />
       </div>
     );
   }
 }
 
-App.defaultProps = {
-  headerProp: " Header from props...",
-  contentProp:"Content from props..."
+class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1> {this.props.headerProp}</h1>
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1> {this.props.contentProp}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
