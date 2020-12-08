60d137d62d76eaae20e02438740b836074181b73
JOGP (Jorge Gustavo Brant Cangussu e Pinho) <JOGP@novonordisk.com>
2020-12-08 15:48:27 -0300

    State

--- aula-01/my-app/src/App.js
+++ aula-01/my-app/src/App.js
@@ -1,17 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from 'react-dom';
 
-function App(props) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Iniciando com ReactJs {props.subject}.
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {
+      header: "Header from state ...",
+      content: "Content from state ..."
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <header><h1>{ this.state.header}</h1></header>
+        <content><h2>{this.state.content}</h2></content>
+      </div>
+    );
+  }
 }
 
 export default App;
