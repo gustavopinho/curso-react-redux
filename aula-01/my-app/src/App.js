ca16acc2755ae4f6f023719384d2da3dcae858e3
JOGP (Jorge Gustavo Brant Cangussu e Pinho) <JOGP@novonordisk.com>
2020-12-09 17:22:40 -0300

    Formulários

--- aula-01/my-app/src/App.js
+++ aula-01/my-app/src/App.js
@@ -6,59 +6,21 @@ class App extends React.Component {
     super();
     
     this.state = {
-      data: 0
+      data: 'Initial data...'
     };
 
-    this.setNewNumber = this.setNewNumber.bind(this);
+    this.updateStatus = this.updateStatus.bind(this);
   }
 
-  setNewNumber () {
-    this.setState({data: this.state.data + 1 });
+  updateStatus (event) {
+    this.setState({ data: event.target.value });
   }
     
   render() {
     return (
       <div>
-        <button onClick = {this.setNewNumber}>INCREMENT</button>
-        <Content myNumber = {this.state.data} />
-      </div>
-    );
-  }
-}
-
-class Content extends React.Component {
-  componentWillMount () {
-    console.log('Component WILL MOUNT!');
-  }
-
-  componentDidMount () {
-    console.log('Component DID MOUNT!');
-  }
-
-  componentWillReceiveProps(newProps) {
-    console.log('Component WILL RECIEVE PROPS!');
-  }
-
-  shouldComponentUpdate(newProps, newState) {
-    return true;
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log('Component WILL UPDATE!');
-  }
- 
-  componentDidUpdate(prevProps, prevState) {
-    console.log('Component DID UPDATE!');
-  }
-
-  componentWillUnmount() {
-    console.log('Component WILL UNMOUNT!');
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>{this.props.myNumber}</h3>
+        <input type="text" value={this.state.data} onChange = {this.updateStatus} />
+        <h4>{ this.state.data }</h4>
       </div>
     );
   }
