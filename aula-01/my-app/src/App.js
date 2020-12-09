e770e1c8effc9a00b51a291860e2733f04313482
JOGP (Jorge Gustavo Brant Cangussu e Pinho) <JOGP@novonordisk.com>
2020-12-09 15:31:56 -0300

    Forçar atualização

--- aula-01/my-app/src/App.js
+++ aula-01/my-app/src/App.js
@@ -3,29 +3,22 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state =  {
-      data: []
-    };
-
-    this.setStateHandle = this.setStateHandle.bind(this);
+  constructor() {
+    super();
+    
+    //this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
   }
 
-  setStateHandle () {
-    let item = "setState ...";
-    let myArray = [...this.state.data];
-    
-    myArray.push(item);
-    this.setState({data: myArray});
+  // Sintax experimental
+  forceUpdateHandler = () => {
+    this.forceUpdate();
   }
     
   render() {
     return (
       <div>
-        <button onClick={this.setStateHandle}>SET STATE</button>
-        <h4> State Array: {this.state.data}</h4>
+        <button onClick={ this.forceUpdateHandler}>FORCE UPDATE</button>
+        <h4> Random number: { Math.random() }</h4>
       </div>
     );
   }
