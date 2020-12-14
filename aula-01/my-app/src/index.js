afbafde8dc36b598a8ccbeea5729b9897f942b67
JOGP (Jorge Gustavo Brant Cangussu e Pinho) <JOGP@novonordisk.com>
2020-12-14 14:54:25 -0300

    Routes

--- aula-01/my-app/src/index.js
+++ aula-01/my-app/src/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, NavLink, Switch } from 'react-router-dom'
 import './index.css';
+
 import App from './App';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+      <h1>React Router Example</h1>
+      <ul>
+        <li>
+          <NavLink to="/" exact activeStyle={{ color: 'red' }}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" exact activeStyle={{color:'green'}}>About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" exact activeStyle={{color:'magenta'}}>Contact</NavLink>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
 );
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ aula-01/my-app/src/pages/NotFound.js
@@ -0,0 +1,3 @@
+import React from 'react'
+const NotFound = () => <h1>404 - Not found</h1>
+export default NotFound
